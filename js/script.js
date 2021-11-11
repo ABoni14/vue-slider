bad6f5643408b7a1b7507bb8955f4ad6bfbc76f5
ABoni14 <aboni150@gmail.com>
2021-11-11 14:15:32 +0100

    add img and use VUE

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const app = new Vue({
+  el: "#app",
+  data:{
+    images: 
+    [
+      'img/01.jpg',
+      'img/02.jpg',
+      'img/03.jpg',
+      'img/04.jpg',
+      'img/05.jpg'
+    ],
+    titles:
+    [
+      'Svezia',
+      'Svizzera',
+      'Gran Bretagna',
+      'Germania',
+      'Paradise'
+    ],
+    text:
+    [
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+      'Lorem ipsum',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+    ],
+    counter: 0
+  },
+
+  methods:{
+    nextImage(){
+      this.counter++;
+      if(this.counter > this.image.length - 1){
+        this.counter = 0;
+      }
+    },
+
+    prevImage(){
+      this.counter--;
+      if(this.counter < 0){
+        this.counter = this.images.length -1;
+      }
+    }
+    
+  }
+
+})
